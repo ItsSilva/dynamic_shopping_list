c1d81b8f1491eeaaee81b4f79fd9dd0be06778e2
Juan Silva <juanse909@hotmail.com>
2024-09-12 23:17:39 -0500

    data

---
+++ public/src/data/ShoppingData.js
@@ -0,0 +1,19 @@
+const shoppingData = [
+    {
+      "name": "Milk",
+      "quantity": 2
+    },
+    {
+      "name": "Bread",
+      "quantity": 1
+    },
+    {
+      "name": "Eggs",
+      "quantity": 12
+    },
+    {
+      "name": "Butter",
+      "quantity": 1
+    }
+  ]
+export default shoppingData;
